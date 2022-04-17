de11f898aa7a549e6e8c0dc30c745de63bab9c22
elaineliuwang <elaine.liuwang@gmail.com>
2022-04-17 00:44:03 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var span = document.getElementsByClassName("close")[1];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+  modal2.style.display = "none";
+  modal3.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal || event.target == modal2 || event.target == modal3) {
+    modal.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+  }
+}
